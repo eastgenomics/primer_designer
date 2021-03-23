d410e1eb1bbaa0ac7d85095336a8f3542a6ed14b
Jethro Rainford <rainford1995@gmail.com>
2021-03-23 23:02:36 +0000

    clean up bulk design

--- bulk_design.py
+++ bulk_design.py
@@ -1,105 +1,94 @@
-#!/usr/bin/python
-# 
-# 
-# 
-# 
-# Kim Brugger (11 Jul 2016), contact: kim@brugger.dk
-# Nikita Povarnitsyn (30 Jul 2019) 
+"""
+Script to call primer_design_region.py to generate primer designs.
+Outputs a zip file of designs.
 
-import sys
+Kim Brugger (11 Jul 2016), contact: kim@brugger.dk
+Nikita Povarnitsyn (30 Jul 2019)
+Jethro Rainford (23 Mar 2021)
+"""
 import os
+from pathlib import Path
+import random
 import re
 import string
-import random
 import subprocess
-
-
-
-
-def system_call( cmd ):
-
-    try:
-        subprocess.check_call(cmd, shell=True)
-
-    except subprocess.CalledProcessError as scall:
-        print("System call '{}' failed. Exit code: {} Error output: {}".format( cmd,  scall.returncode, scall.output))
-        exit()
-
-
-
-# Define system call
-
-if len(sys.argv ) == 1:
-	print("USAGE: bulk_design.py input-file [working dir]")
-	print("input-file one tab seperated output-name, region and reference (grch37 or grch38) of interest pr line")
-	exit()
-
-infile = sys.argv[1]
-if infile.find(".txt") == -1:
-	print("Infile '{}' does not look like a txt file".format(infile))
-	exit()
-
-
-if len(sys.argv ) == 3:
-    os.chdir( sys.argv[ 2 ])
-
-
-outfile = re.sub(r'.txt', '.zip', infile)
-
-# Run designer function 
+import sys
 
 
 def random_string(length=10):
-    """ Creates a random string 
+    """
+    Creates a random string, used to name out dir uniquely
 
     Args:
       length (int): length of string to generate, default 10
 
     returns:
-      string
-
-    raises:
-      None
+        random_string (str): random character string
     """
-
     random_string = ""
-    # Choose from lowercase, uppercase,  and digits
+
+    # Choose from lowercase, uppercase and digits
     alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits
     for n in range(0, length):
-        random_string += random.choice( alphabet )
+        random_string += random.choice(alphabet)
 
     return random_string
 
+
 if __name__ == '__main__':
 
+    if len(sys.argv) == 1:
+        print("USAGE: bulk_design.py input-file [working dir]")
+        print((
+            "input-file one tab seperated output-name, region and reference "
+            "(grch37 or grch38) of interest pr line"
+        ))
+        sys.exit()
+
+    infile = sys.argv[1]
+    if infile.find(".txt") == -1:
+        print("Infile '{}' does not look like a txt file".format(infile))
+        sys.exit()
+
+    if len(sys.argv) == 3:
+        os.chdir(sys.argv[2])
+
+    outfile = re.sub(r'.txt', '.zip', infile)
 
     working_dir = random_string()
-    os.mkdir( working_dir )
-    os.chdir( working_dir )
+    os.mkdir(working_dir)
+    os.chdir(working_dir)
 
+    primer_designer_path = \
+        f"{Path(__file__).parent.absolute()}/primer_designer_region.py"
 
-    # Main Loop
-    with open( "../{}".format( infile ) , "rU" ) as plist:
-	
-	for line in plist:
-
-            print(line)
+    with open("../{}".format(infile), "rU") as plist:
+        # loop over input txt file of regions to design primers for and
+        # call primer_designer_region for each
+        for line in plist:
             line = line.strip("\n")
             if line == '':
-                continue 
+                continue
 
             testname, region, reference = re.split(r"[\t ]+", line)
-
             reference = reference.lower()
-
             chrom, npos = region.split(":")
-            cmd = "/mnt/storage/apps/software/primer_designer/1.1/primer_designer_region.py -c {} -p {} -o {} --{}".format(chrom, npos, testname, reference)
-            print(cmd) 
-            system_call(cmd )
 
+            cmd = (
+                f"{primer_designer_path} -c {chrom} -p {npos} -o {testname} "
+                f"--{reference}"
+            )
 
-    os.chdir( ".." )
-    system_call( "zip -j {} {}/*pdf".format( outfile, working_dir ))
+            try:
+                # call primer designer with formatted command
+                subprocess.check_call(cmd, shell=True)
+            except subprocess.CalledProcessError as call:
+                print((
+                    f"System call '{cmd}' failed. Exit code: {call.returncode}"
+                    f" Error output: {call.output}"
+                ))
+
+    os.chdir("..")
+    system_call("zip -j {} {}/*pdf".format(outfile, working_dir))
 
     print("SUCCESS\nOutput file: {}".format(outfile))
-    
